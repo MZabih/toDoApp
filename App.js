b06c467bfdc03a4afe15ffdab9376744ac27b42e
Muhammad ZabihUllah <xabikayani@Muhammads-MacBook-Pro.local>
2020-12-09 02:13:44 +0500

    taskCompleted

--- App.js
+++ App.js
@@ -1,45 +1,40 @@
 import React from 'react';
-import { StyleSheet, View, Button, TouchableOpacity, Dimensions, ScrollView, Animated, Alert, AlertIOS, FlatList, Platform } from 'react-native';
-import { Container, Header, Title, Content, Icon, button, Card, CardItem, Text, Body, Left, Right, IconNB, Footer } from "native-base";
-// import { MaterialIcons, MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons'
+import { StyleSheet, View, TouchableOpacity, Dimensions, TextInput, FlatList, Modal, TouchableHighlight, LogBox } from 'react-native';
+import { Container, Header, Title, Content, Icon, button, Card, CardItem, Text, Body, IconNB, Footer } from "native-base";
 import moment from 'moment';
-// import prompt from 'react-native-prompt-android';
-// import Expo from 'expo';
 
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 
-
-
 export default class Todo extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       text: '',
+      taskName: '',
+      modalVisible: false,
       data: [
         'Java', 'Python', 'Javascript'
-      ],
-      loading: false
+      ]
     }
-    this.add = this.add.bind(this);
-    this.remove = this.remove.bind(this);
-    this.showForm = this.showForm.bind(this);
+    LogBox.ignoreAllLogs();
   }
 
-  async componentWillMount() {
-    // await Expo.Font.loadAsync({
-    //   Roboto: require("native-base/Fonts/Roboto.ttf"),
-    //   Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-    //   Ionicons: require("native-base/Fonts/Ionicons.ttf")
-    // });
-    this.setState({ loading: true });
+  // show-Hide-Modal
+
+  setModalVisible = (visible) => {
+    const {taskName} = this.state
+    this.addTask(taskName)
+    this.setState({ modalVisible: visible, taskName : ''});
   }
 
-  add = (text) => {
+  // Add Task
+
+  addTask = (text) => {
 
     let notEmpty = text.trim().length > 0;
 
     if (notEmpty) {
-
       this.setState(
         prevState => {
           let { data } = prevState;
@@ -52,8 +47,9 @@ export default class Todo extends React.Component {
     }
   }
 
-  remove = (i) => {
+  //Remove Task
 
+  remove = (i) => {
     this.setState(
       prevState => {
         let data = prevState.data.slice();
@@ -63,118 +59,100 @@ export default class Todo extends React.Component {
         return { data };
       }
     );
-
   }
 
-  // updateState =
-  showForm = () => {
+  // onChangeText of Modal
 
-    if (Platform.OS == 'android') {
+   handleTaskName = (text) => {
+      this.setState({ taskName: text })
+   }
 
-      prompt(
-        'Enter Text',
-        [
-          { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
-          { text: 'OK', onPress: text => this.add(text) },
-        ],
-        {
-          type: 'secure-text',
-          cancelable: false,
-          defaultValue: 'test',
-          placeholder: 'placeholder'
-        }
-      );
-    }
+  // Render Modal View
 
-    else {
-      AlertIOS.prompt(
-    'Enter password',
-    'Enter your password to claim your $1.5B in lottery winnings',
-    [
-      {
-        text: 'Cancel',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
-      },
-      {
-        text: 'OK',
-        onPress: (password) => console.log('OK Pressed, password: ' + password),
-      },
-    ],
-    'secure-text'
-  );
-    }
+  showModal = () => {
 
+    const { modalVisible } = this.state;
+    return (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Enter Task to do</Text>
+              <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Task Name"
+               autoCapitalize = "none"
+               onChangeText = {(text) => {this.handleTaskName(text)}}/>
+              <TouchableHighlight
+                style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+                onPress={() => {
+                  this.setModalVisible(!modalVisible);
+                }}
+              >
+                <Text style={styles.textStyle}>Done</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+    );
   }
-
-
 
   render() {
-
-    // if (!this.state.loading) {
-    //   return <Expo.AppLoading />
-    // }
-
     return (
       <Container style={{}}>
+      {this.showModal()}
         <Header>
-          <Left>
-            <Title>{moment().format('MMMM Do YYYY')}</Title>
-          </Left>
+          <View style = {{top:20,left:0}}>
+            <Title>{moment().format("MMM Do YYYY")}</Title>
+          </View>
           <Body>
 
           </Body>
-          <Right>
+          <View style = {{top:20,right:0}}>
             <Body>
-              <Title>{moment().format('dddd')}</Title>
+              <Title>{moment().format('ddd')}</Title>
             </Body>
-          </Right>
+          </View>
         </Header>
         <Content>
-
           <FlatList
             data={this.state.data}
             renderItem={({ item, index }) =>
-
-
               <Card key={index}>
-                <CardItem key={index} style={{ height: 50 }}>
+                <CardItem key={index} style={{ height: 50}}>
                   <Body>
                     <Text>
                       {item}
                     </Text>
                   </Body>
-                  <Right>
+                  <View>
                     <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', padding: 5, borderRadius: 5, borderColor: '#32CD32' }}
                       onPress={() => this.remove(index)}>
+                      <Text> - </Text>
                     </TouchableOpacity>
-                  </Right>
+                  </View>
                 </CardItem>
               </Card>
-
             }
             keyExtractor={item => item.toString()}
-
           />
-
-
         </Content>
         <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-          <TouchableOpacity style={{ backgroundColor: '#32CD32', alignItems: 'center', justifyContent: 'center', padding: 20, borderRadius: 100 }}
-            onPress={() =>
-              this.showForm()
-            }>
 
+          <TouchableOpacity style={styles.showModal}
+            onPress={() =>
+              this.setModalVisible(true)
+            }>
+            <Text style = {{color: '#fff'}}> + </Text>
           </TouchableOpacity>
         </View >
       </Container>
-
     );
   }
 }
-
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -183,4 +161,51 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+   input: {
+      margin: 15,
+      height: 40,
+   },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  showModal: {
+     backgroundColor: '#32CD32',
+     alignItems: 'center',
+     justifyContent: 'center',
+     padding: 20,
+     borderRadius: 200
+  }
 });
